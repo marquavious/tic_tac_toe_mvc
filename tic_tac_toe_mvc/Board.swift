ccfc25793a0743d1224033fad227dce91c513203
Marquavious Draggon <marq.draggon@gmail.com>
2016-12-16 08:35:20 -0800

    changed colord

--- tic_tac_toe_mvc/Board.swift
+++ tic_tac_toe_mvc/Board.swift
@@ -11,7 +11,7 @@ import UIKit
 class Board: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .green
+        self.backgroundColor = .red
         self.translatesAutoresizingMaskIntoConstraints = false
     }
     
